940563d9b3e3ac65186b6183c238abfaf986fa19
Megha Havaragi <meghahavaragi@gmail.com>
2019-04-10 10:15:28 +0530

    replace curl with requests in logging

--- payload/tmp/logging/SumologicHandler.py
+++ payload/tmp/logging/SumologicHandler.py
@@ -1,7 +1,6 @@
-import logging
 import logging.handlers
 import subprocess
-
+import requests
 
 class SumologicHandler(logging.handlers.HTTPHandler):
     def emit(self, record):
@@ -9,5 +8,14 @@ class SumologicHandler(logging.handlers.HTTPHandler):
 
 #        log_url = f'https://{self.host}{self.url}'
         log_url = 'https://{}{}'.format(self.host, self.url)
-        command = ['curl', '-s', '-X', 'POST', log_url, '-H', 'content-type: application/json', '-d', log_entry]
-        return subprocess.check_output(command)
+        headers = {
+            'Cache-Control': "no-cache",
+            'Content-Type': "application/json"
+        }
+        response = requests.post(log_url, data=log_entry, headers=headers)
+
+
+        #command = ['curl', '-s', '-X', 'POST', log_url, '-H', 'content-type: application/json', '-d', log_entry]
+        #return subprocess.check_output(command)
+
+        return response
